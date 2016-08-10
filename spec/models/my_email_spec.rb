859a21fdef2b166d4aac63562dc2dd6df32c28dd
chocko <malo_s8s1@hotmail.com>
2016-08-09 22:54:00 -0500

    Form buildes v18

--- spec/models/my_email_spec.rb
+++ spec/models/my_email_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe MyEmail, type: :model do
   it{ should validate_presence_of(:email).with_message("Woops! vacio")}
-  it{ should validate_uniqueness_of(:email).with_message("Woops! vacio")}
+  it{ should validate_uniqueness_of(:email).with_message("Este correo ya fue registrado")}
 
   it "should not create with invalid email" do
       email = MyEmail.new(email:"chocko")
