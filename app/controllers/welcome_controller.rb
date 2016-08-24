0041dd743871b7a6da551b65b28d904498a24210
chocko <malo_s8s1@hotmail.com>
2016-08-23 21:02:43 -0500

    Modelo link descargas  v46

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,11 @@ class WelcomeController < ApplicationController
     render layout: "landing"
   end
 
+  def payment_succed
+    if @shopping_cart.payed?
+      cookies[:shopping_cart_id] = nil
+    else
+      redirect_to checkout_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get "/carrito", to: "shopping_carts#show"
   get "/add/:product_id", as: :add_to_cart,to: "in_shopping_carts#create"
   get "/checkout",to: "payments#checkout"
+
+  get "/ok", to: "welcome#payment_succed"
+
   authenticated :user do
     root 'welcome#index'
   end
